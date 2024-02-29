79c5d9c35feaeb8d0b2bf643401ff3f68551f57a
Delcious <151561639+Delcious@users.noreply.github.com>
2024-02-29 16:21:14 -0500

    Update customers.py

--- customers.py
+++ customers.py
@@ -1,16 +1,22 @@
-import argparse
 import json
+def read_orders(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filename', nargs='?', default='example_orders.json')
-args = parser.parse_args()
+def extract_customer_data(orders):
+    customer_data = {}
+    for order in orders:
+        name = order.get("name")
+        phone = order.get("phone")
+        if name and phone:
+            customer_data[name] = phone
+    return customer_data
 
-customer = {}
-with open (args.filename,'r') as f:
-    data = json.load(f)
-    for order in data:
-        name =order["name"]
-        phone = order ["phone"]
-        customer[name] = phone
-with open('customers.json','w') as f:
-    json.dump(customer,f)
+def main():
+    orders = read_orders('example_orders.json')
+    customer_data = extract_customer_data(orders)
+    with open('customers.json', 'w') as f:
+        json.dump(customer_data, f) 
+
+if __name__ == "__main__":
+    main()
