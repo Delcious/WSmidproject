bd0a62d61d13590108b7e288641ae5371bdce6f0
Delcious <151561639+Delcious@users.noreply.github.com>
2024-03-01 01:48:16 -0500

    Update items.py

--- items.py
+++ items.py
@@ -1,1 +1,12 @@
-
+import json
+def read_orders():
+  
+def process_items():
+  
+def main():
+  orders = read_orders('example_orders.json')
+  items = process_items(orders)
+  with open('items.json', 'w') as f:
+        json.dump(items, f)
+if __name__ =="__main__":
+  main()
